f427dd01cec6267977791dea0e83eda612cadb55
LEADER <godstark82@gmail.com>
2024-08-10 11:36:06 +0530

    git hooks test

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
+void main(List<String> args) {
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        home: Scaffold(body: Center(child: Text('Hello World!'))));
+    return const MaterialApp();
   }
 }
